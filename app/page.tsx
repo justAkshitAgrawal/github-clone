7ebd015ad7e29524912d055e741de84cfdfb4592
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2023-08-02 20:32:31 +0530

    Hero section built

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 export default function Home() {
   return (
@@ -11,13 +12,16 @@ export default function Home() {
       </div>
       <div>
         <div className="overflow-hidden">
-          <div className="relative h-screen">
+          <div className="relative ">
             <img
               className="absolute top-0 transition duration-200 ease-in max-xl:right-[-1050px] xl:right-[-970px] image"
               src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
               alt=""
               width="4377"
             />
+          </div>
+          <div className="px-3 Hero-section">
+            <Hero />
           </div>
         </div>
       </div>
